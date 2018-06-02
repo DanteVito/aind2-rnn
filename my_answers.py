d125625aff126691cf859029b068665cec104fbf
Ubuntu <ubuntu@ip-172-31-1-224.ec2.internal>
2018-06-02 15:12:58 +0000

    this is the first submission version

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
@@ -12,35 +13,56 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+   
+    l = len(series) 
+    
+    for i in range(0, l-window_size):
+        X.append(series[i:window_size+i])
+        y.append(series[window_size+i])
+    
     # reshape each 
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    #X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    #y.shape = (len(y),1)
 
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    new_text = ''
+    for char in text:
+        if char not in punctuation:
+            new_text += char
+    return new_text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    l = len(text)
+    for i in range(0, l-window_size, step_size):
+        inputs.append(text[i:window_size+i])
+        outputs.append(text[window_size+i])
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    return model
